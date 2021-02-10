9a142efbc7cd94018b421af79851c27aefdbd9c7
RakshithA1996 <rakshithacharya75@gmail.com>
2021-02-10 07:18:08 +0530

    bookshlef commits new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import blackSearch from "../images/blackSearch.png";
-import { HeaderWrapper } from '../styles/components/HeaderStyle';
+import { HeaderWrapper, HeaderButtonWrap } from '../styles/components/HeaderStyle';
 
 export const Header = (props) => {
     return (
         <HeaderWrapper isBorder={props.isBorder}>
             <div className="logo1">BOOK<span className="logo2">shelf</span></div>
-            <div className="search">
+            {/* <div className="search">
                 <input className="searchBar" placeholder="search . . ." />
                 <img className="searchImg" src={blackSearch} alt="blackSearch" />
-            </div>
+            </div> */}
+            <HeaderButtonWrap>
+                
+                <button className="signUpHeader">Sign Up</button>
+                <button className="loginHeader">Login</button>
+            </HeaderButtonWrap>
         </HeaderWrapper>
     )
 }
--- src/styles/components/HeaderStyle.js
+++ src/styles/components/HeaderStyle.js
@@ -6,6 +6,10 @@ const lightTheme = {
     logo2:"#383838",
     searchColor:"#000000",
     searchBorder:"1px solid #383838",
+    signUpBg:"#f0912b",
+    signUpColor:"#ffffff",
+    loginBg:"#b50000",
+    loginColor:"#ffffff",
 } 
 
 export const HeaderWrapper = styled.div`
@@ -16,13 +20,14 @@ export const HeaderWrapper = styled.div`
     position:fixed;
     top:0;
     width:100%;
-    padding: 2rem 0;
+    padding: 1.2rem 0;
     background: ${lightTheme.headerBg};
     box-shadow: ${props => props.isBorder ? "0 8px 6px -6px #a1a1a1" : "none"};
     
     .logo1 {
+        width: 50%;
         font-family: 'Anton', sans-serif;
-        font-size:5rem;
+        font-size:3.5rem;
         font-weight:600;
         letter-spacing: 0.2rem;
         line-height:100%;
@@ -32,11 +37,11 @@ export const HeaderWrapper = styled.div`
     }
 
     .logo2 {
-        font-size:3rem;
+        font-size:2.5rem;
         color: ${lightTheme.logo2};
     }
 
-    .search {
+    /* .search {
         display:flex;
         flex-direction:row;
         align-items:center;
@@ -71,7 +76,45 @@ export const HeaderWrapper = styled.div`
 
     .searchImg:hover {
         cursor: pointer;
+    } */
+`;
+
+export const HeaderDarkWrap = styled.div`
+`;
+
+export const HeaderButtonWrap = styled.div`
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:flex-end;
+    margin-right:4rem;
+    width: 50%;
+    font-size: 1.3rem;
+
+    .signUpHeader {
+        width: 13%;
+        border:none;
+        padding: 0.8rem 0rem;
+        border-radius: 5px;
+        background: ${lightTheme.signUpBg};
+        color: ${lightTheme.signUpColor};
     }
 
-    
+    .loginHeader {
+        width: 13%;
+        border:none;
+        padding: 0.8rem 0rem;
+        border-radius: 5px;
+        margin-left: 1rem;
+        background: ${lightTheme.loginBg};
+        color: ${lightTheme.loginColor};
+    }
+
+    .signUpHeader:hover, .loginHeader:hover {
+        cursor: pointer;
+    }
+
+    .signUpHeader:focus, .loginHeader:focus {
+       outline:none;
+    }
 `;
